9a882f0720bf2dd6e50cd7074613c6cf3180bff4
marlisapee <mpee@tripadvisor.com>
2024-10-29 15:11:06 -0400

    initial commit

---
+++ src/main/java/com/marlisajp/booktokapi/BooktokapiApplication.java
@@ -0,0 +1,13 @@
+package com.marlisajp.booktokapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooktokapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooktokapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marlisajp/booktokapi/BooktokapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marlisajp.booktokapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooktokapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
