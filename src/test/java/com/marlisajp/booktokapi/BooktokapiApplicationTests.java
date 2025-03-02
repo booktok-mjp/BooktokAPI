a60626fb17735d856183d7dd37d889cf70d0fb83
marlisapee <mpee@tripadvisor.com>
2025-03-01 22:53:53 -0500

    testing workflow

--- src/test/java/com/marlisajp/booktokapi/BooktokapiApplicationTests.java
+++ src/test/java/com/marlisajp/booktokapi/BooktokapiApplicationTests.java
@@ -1,16 +1,17 @@
 package com.marlisajp.booktokapi;
 
-import com.marlisajp.booktokapi.config.TestConfigurationConfig;
+import com.marlisajp.booktokapi.config.TestSecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.context.annotation.Import;
 
-@SpringBootTest(classes = TestConfigurationConfig.class)
+@SpringBootTest
 @ActiveProfiles("test")
+@Import(TestSecurityConfig.class)
 class BooktokapiApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
