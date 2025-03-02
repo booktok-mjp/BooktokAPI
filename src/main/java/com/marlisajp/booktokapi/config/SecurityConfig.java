91ee5bb9420c9bd13b9cc053289aa6626b11b5ef
marlisapee <mpee@tripadvisor.com>
2025-03-01 22:56:49 -0500

    testing workflow

--- src/main/java/com/marlisajp/booktokapi/config/SecurityConfig.java
+++ src/main/java/com/marlisajp/booktokapi/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.marlisajp.booktokapi.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -15,6 +16,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
+@Profile("!test")
 public class SecurityConfig {
 
     @Value("${AUTH0_ISSUER_URI:}")
