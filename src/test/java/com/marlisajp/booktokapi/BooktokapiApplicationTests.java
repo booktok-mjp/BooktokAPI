2faeac985e51c11465a06b374b56eb380db99b87
marlisapee <mpee@tripadvisor.com>
2025-03-01 22:31:56 -0500

    testing workflow

--- src/test/java/com/marlisajp/booktokapi/BooktokapiApplicationTests.java
+++ src/test/java/com/marlisajp/booktokapi/BooktokapiApplicationTests.java
@@ -1,12 +1,11 @@
 package com.marlisajp.booktokapi;
 
+import com.marlisajp.booktokapi.book.BookTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(properties = {
-		"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration"
-})
+@SpringBootTest(classes = BookTest.class,properties = "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration")
 @ActiveProfiles("test")
 class BooktokapiApplicationTests {
 
